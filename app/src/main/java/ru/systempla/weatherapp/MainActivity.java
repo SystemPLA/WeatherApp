bb965822a52b2e3b5b30b75834da530ef6d4a75a
SystemPLA <amalovatyj@yandex.ru>
2019-10-23 15:07:00 +0300

    накидал tool bar пока не работает

--- app/src/main/java/ru/systempla/weatherapp/MainActivity.java
+++ app/src/main/java/ru/systempla/weatherapp/MainActivity.java
@@ -1,16 +1,27 @@
 package ru.systempla.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
 
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
 
+    private Toolbar toolbar;
+    private EditText searchEditText;
     private Fragment fragmentCities;
     private Fragment fragmentSettingButton;
     private Fragment fragmentSettings;
@@ -41,6 +52,8 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+
         fragmentCities = new CitiesFragment();
         fragmentSettingButton = new SettingsButtonFragment();
         fragmentSettings = new SettingsFragment();
@@ -54,5 +67,65 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
 
     public SettingsParcel getSettingsParcel(){
         return this.settingsParcel;
+    }
+
+    private void initViews() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        searchEditText = findViewById(R.id.searchEditText);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        handleMenuItemClick(item);
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void handleMenuItemClick(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.change_settings: {
+                Toast.makeText(getApplicationContext(), "Изменение настроек", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.change_city: {
+                if(searchEditText.getVisibility() == View.VISIBLE) {
+                    searchEditText.setVisibility(View.GONE);
+                    Objects.requireNonNull(getSupportActionBar())
+                            .setTitle(getString(R.string.app_name));
+                } else {
+                    Objects.requireNonNull(getSupportActionBar()).setTitle("");
+                    searchEditText.setVisibility(View.VISIBLE);
+                    searchEditText.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                            //Вам приходит на вход текст поиска - ищем его - в бд, через АПИ (сервер в инете) и т.д.
+                            Toast.makeText(getApplicationContext(), s.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                break;
+            }
+            default: {
+                Toast.makeText(getApplicationContext(), getString(R.string.action_not_found),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
