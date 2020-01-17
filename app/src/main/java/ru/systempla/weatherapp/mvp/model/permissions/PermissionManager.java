0f4c026cb30d7f5b11c8272d7abc38cd6ae43335
SystemPLA_p <amalovatyj@yandex.ru>
2020-01-17 22:28:36 +0300

    WIP 17-1-20 22-28

--- app/src/main/java/ru/systempla/weatherapp/mvp/model/permissions/PermissionManager.java
+++ app/src/main/java/ru/systempla/weatherapp/mvp/model/permissions/PermissionManager.java
@@ -1,22 +1,36 @@
 package ru.systempla.weatherapp.mvp.model.permissions;
 
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+
+import java.lang.reflect.Array;
+
+import ru.systempla.weatherapp.mvp.App;
+
 public class PermissionManager implements IPermissionManager {
 
     @Override
     public boolean checkPermission(String permission) {
-        return false;
+        ActivityCompat.checkSelfPermission(App.getInstance(), permission);
+        return (ActivityCompat.checkSelfPermission(App.getInstance(), permission)!= PackageManager.PERMISSION_GRANTED);
     }
 
+    @Override
     public boolean checkPermission(String ... permissions){
-
+        boolean flag = true;
+        for (String permission : permissions ) {
+            flag &= (ActivityCompat.checkSelfPermission(App.getInstance(), permission)!= PackageManager.PERMISSION_GRANTED);
+        }
+        return flag;
     }
 
     @Override
     public void getPermission(String permission) {
-
+        (, new String[]{permission}, 100);
     }
 
+    @Override
     public void getPermission(String ... permissions){
-        
+        ActivityCompat.requestPermissions(this, permissions,100);
     }
 }
