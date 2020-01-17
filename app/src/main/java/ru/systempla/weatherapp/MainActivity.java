830b2dfab9c4acdd1642b1de4518750cba64c86c
SystemPLA <amalovatyj@yandex.ru>
2020-01-18 02:41:25 +0300

    UI 18-1-20 2-41

--- app/src/main/java/ru/systempla/weatherapp/MainActivity.java
+++ app/src/main/java/ru/systempla/weatherapp/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_old);
 
         initViews();
         initSideMenu();
