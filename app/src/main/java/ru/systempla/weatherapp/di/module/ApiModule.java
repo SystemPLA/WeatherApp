65178c425d962fc126cc6367112966b1865c49b6
SystemPLA <amalovatyj@yandex.ru>
2020-02-06 13:55:40 +0300

    starting on 19

--- app/src/main/java/ru/systempla/weatherapp/di/module/ApiModule.java
+++ app/src/main/java/ru/systempla/weatherapp/di/module/ApiModule.java
@@ -1,12 +1,7 @@
 package ru.systempla.weatherapp.di.module;
 
-import javax.inject.Named;
-import javax.inject.Singleton;
-
 import dagger.Module;
 import dagger.Provides;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,31 +12,12 @@ import ru.systempla.weatherapp.mvp.model.api.IDataSource;
 public class ApiModule {
 
     @Provides
-    public IDataSource api(@Named("clientLogging") OkHttpClient okHttpClient) {
+    public IDataSource api() {
         return new Retrofit.Builder()
                 .baseUrl("https://api.openweathermap.org/")
-                .client(okHttpClient)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(IDataSource.class);
-    }
-
-    @Singleton
-    @Provides
-    public HttpLoggingInterceptor loggingInterceptor() {
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.level(HttpLoggingInterceptor.Level.BODY);
-        return interceptor;
-    }
-
-
-    @Named("clientLogging")
-    @Singleton
-    @Provides
-    public OkHttpClient okHttpClientLogging(HttpLoggingInterceptor loggingInterceptor) {
-        return new OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .build();
     }
 }
